59aa545e8610509a611e1e45822ecefa488121d4
eyrooonnn <espinosa.aaron45@gmail.com>
2022-10-29 14:46:43 +0800

    added app structure

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <View style={styles.timeContainer}>
+          <Text>3:00 PM</Text>
+          <Text>October 29, 2022</Text>
+        </View>
+        <View style={styles.socMedContainer}>
+          <TouchableOpacity style={styles.icon}>
+            <Text>FB</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon}>
+            <Text>Twitter</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.icon}>
+            <Text>Insta</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- styles.js
+++ styles.js
@@ -1,12 +1,43 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
     },
-  });
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+    },
+    timeContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 0.2,
+        width: '100%',
+        backgroundColor: 'green',
+    },
+    socMedContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 0.8,
+        width: '100%',
+        backgroundColor: 'pink',
+    },
+    icon: {
+        width: 60,
+        height: 60,
+        margin: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'salmon',
+    },
+});
 
 export { styles }
